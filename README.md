28fc6407001e1402eb151ba0d0f79f0b98e8d3aa
Sheeba <sheeba.s@Ltts.com>
2021-05-18 00:01:20 -0700

    initial_Commits

---
+++ README.md
@@ -0,0 +1,1 @@
+# WPFFinalActivity
---
+++ ServerChat/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+
+
+namespace ServerChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8080/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server running at {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR("/signalchat", new HubConfiguration());
+            GlobalHost.Configuration.MaxIncomingWebSocketMessageSize = null;
+        }
+    }
+}
